8baeb2ae17317540bd9dbfcf5dba55da9fc7269b
LOrangex <lethan0920@gmail.com>
2022-02-27 21:43:19 +0800

    Update 004.py

--- 004.py
+++ 004.py
@@ -24,6 +24,9 @@ model = Sequential()
 model.add(Conv2D(40, kernel_size=(3, 3), padding="same",
                  input_shape=X_train.shape[1:], activation="relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(40, kernel_size=(3, 3), padding="same",
+                 activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
 model.add(Dense(512, activation="relu"))
